c3211e15ecb3699f819c8597bb0b84a41820414a
kaka18mika <kaka18mika@gmail.com>
2024-05-31 13:50:24 -0300

    desafio criado

--- desafioFelipao.js
+++ desafioFelipao.js
@@ -1,5 +1,34 @@
-function calculadoraPartidasRankd(derrostas, vitorias){
-    let vitorias = 0
-    let derrostas = 0
-    
+function calculadoraPartidasRankd(derrotas, vitorias){
+    let calcular = vitorias - derrotas
+    return calcular
 }
+
+let resultado = calculadoraPartidasRankd(10, 80)
+let rank 
+
+while (true){ 
+    if (resultado < 10){
+        rank = ("Ferro")
+    }
+    else if (resultado > 10 && resultado <= 20){
+        rank = ("Bronze")
+    }
+    else if (resultado > 20 && resultado <= 50){
+        rank = ("Prata")
+    }
+    else if (resultado > 50 && resultado <= 80){
+        rank = ("Ouro")
+    }
+    else if (resultado > 80 && resultado <=90){
+        rank = ("Diamante")
+    }
+    else if (resultado > 90 && resultado <= 100){
+        rank = ("Lendário")
+    }
+    else if (resultado >= 101){
+        rank = ("Imortal")
+    }
+    break
+}
+
+console.log("O herói tem um saldo de " + (resultado) + " vitórias e está no nível " + (rank))
